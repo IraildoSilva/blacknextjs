6227d257cfac95eaf36c060d9650c459fd19a139
Iraildo Silva <iraildosilva1999@gmail.com>
2023-10-29 10:33:43 -0300

    testing auto rerendering with ISR

--- src/pages/static.tsx
+++ src/pages/static.tsx
@@ -1,66 +1,59 @@
-import { GetServerSideProps, GetStaticProps, NextPage } from "next";
-import { ReactNode, useEffect, useState } from "react";
+import { GetServerSideProps, GetStaticProps, NextPage } from 'next'
+import { ReactNode, useEffect, useState } from 'react'
 import { Col, Container, Row } from 'reactstrap'
 
 interface IApiResponse {
-	name: string
-	timestamp: Date
+  name: string
+  timestamp: Date
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-	const staticData =
-		await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/hello`)
-			.then(res => res.json())
+  const staticData = await fetch(
+    `${process.env.NEXT_PUBLIC_APIURL}/api/hello`
+  ).then((res) => res.json())
 
-	return {
-		props: {
-			staticData
-		},
-		revalidate: 10
-	}
+  return {
+    props: {
+      staticData,
+    },
+    revalidate: 10,
+  }
 }
 
 const Static: NextPage = (props: {
-	children?: ReactNode
-	staticData?: IApiResponse
+  children?: ReactNode
+  staticData?: IApiResponse
 }) => {
-	const [clientSideData, setClienteSideData] = useState<IApiResponse>()
+  const [clientSideData, setClienteSideData] = useState<IApiResponse>()
+  const [staticDataISR, setStaticDataISR] = useState<string | undefined>('')
 
-	useEffect(() => {
-		async function fetchData() {
-			const data = await fetch('/api/hello').then(result => result.json())
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetch('/api/hello').then((result) => result.json())
 
-			setClienteSideData(data)
-		}
+      setClienteSideData(data)
+    }
 
-		fetchData()
-	}, [])
+    setStaticDataISR(props.staticData?.timestamp.toString())
+    fetchData()
+  }, [props])
 
-	return (
-		<Container tag={'main'}>
-			<h1 className="my-5">
-				Como funcionam as renderizações no nextJS
-			</h1>
+  return (
+    <Container tag={'main'}>
+      <h1 className="my-5">Como funcionam as renderizações no nextJS</h1>
 
-			<Row>
-				<Col>
-					<h3>
-						Gerado no estaticamente durante o build:
-					</h3>
-					<h2>{props.staticData?.timestamp.toString()}</h2>
-				</Col>
-				<Col>
-					<h3>
-						Gerado no cliente:
-					</h3>
-					<h2>
-						{clientSideData?.timestamp.toString()}
-					</h2>
-				</Col>
-			</Row>
-
-		</Container>
-	)
+      <Row>
+        <Col>
+          <h3>Gerado no estaticamente durante o build:</h3>
+          <h2>{staticDataISR}</h2>
+        </Col>
+        <Col>
+          <h3>Gerado no cliente:</h3>
+          <h2>{clientSideData?.timestamp.toString()}</h2>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default Static
